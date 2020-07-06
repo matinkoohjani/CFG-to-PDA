bad84fef98e80e5d1153f42ef98cda6e07e16d1d
Ali Nakhaee <ali.sh7097@gmail.com>
2020-07-06 14:33:33 +0430

    second commit same but with more user friendly Main class :))))

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,33 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the address of the file which contains grammar in Greibach form: ");
+        String fileAddress = scanner.next();
+        System.out.println("\n*****************\n");
         PushDownAutomata pda = new PushDownAutomata();
-        pda.setTransitions("a.txt");
-        pda.find("Q0", "aqppcp", "z");
-        System.out.println(pda.isAccepted);
-//        String grammar = "S->aSPCP|aSPP|q\n" + "C->c\n" + "P->p\n";
-//        Scanner scanner = new Scanner(grammar);
+        pda.setTransitions(fileAddress);
+        System.out.println("\nTransitions has been recognized\n\n******************\n");
 
-//        while (scanner.hasNext()){
-//            String temp = scanner.next();
-//            String currentStackTop = temp.substring(0,1);
-//            int lastIndex = 0, secondLastIndex=3;
-//
-//            while (lastIndex != -1) {
-//                lastIndex = temp.indexOf("|", lastIndex);
-//                if (lastIndex != -1) {
-//                    String input = temp.substring(secondLastIndex,secondLastIndex+1);
-//                    String nextStackTop = temp.substring(secondLastIndex+1, lastIndex);
-//                    transitions.add(new Transition(Q1, input, currentStackTop, Q1, nextStackTop));
-//                    secondLastIndex = ++lastIndex;
-//                }
-//            }
-//            String input = temp.substring(secondLastIndex,secondLastIndex+1);
-//            String nextStackTop = temp.substring(secondLastIndex+1);
-//            transitions.add(new Transition(Q1, input, currentStackTop, Q1, nextStackTop));
-//        }
-//
-//        for( Transition transition : transitions)
-//            System.out.println(transition);
+        while (!fileAddress.equals("no")){
+            System.out.println("Enter a string to check if it is accepted or not : ");
+            String input = scanner.next();
+            System.out.println();
+            pda.find("Q0", input, "z");
+            if(pda.isAccepted)
+                System.out.println(input + " is ACCEPTED\n");
+            else
+                System.out.println(input + " is REJECTED\n");
+            pda.isAccepted = false;
+            System.out.println("Want to check more strings ? (yes/no)");
+            fileAddress = scanner.next();
+            System.out.println();
+        }
+
     }
 }
